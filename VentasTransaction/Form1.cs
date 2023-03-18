2d3b8f45ff029bf968d880b3ad29f62c193202df
Raymundo Daniel Ramírez Ramos <ray.ramirez@unikino.edu.mx>
2023-03-18 13:51:30 -0700

    ConnectionString

--- VentasTransaction/Form1.cs
+++ VentasTransaction/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AccesoDatos;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,16 @@ namespace VentasTransaction
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(Conexion.ConnectionString);
         }
     }
 }
